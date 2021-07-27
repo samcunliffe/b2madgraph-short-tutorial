feafa0c00754784025040008508e42798e5606f6
Sam Cunliffe <sam.cunliffe@desy.de>
2021-07-27 18:39:20 +0200

    The mg5_aMC executable is in the path.

--- 0_setup.sh
+++ 0_setup.sh
@@ -2,7 +2,6 @@
 
 . /cvmfs/belle.cern.ch/tools/b2setup release-05-02-12
 
-export MGPATH=$BELLE2_EXTERNALS_DIR/Linux_x86_64/common/bin/mg5_aMC
 export MODELPATH=$BELLE2_RELEASE_DIR/generators/madgraph/models/ALP_UFO
 echo $MODELPATH
 
